464d52dc48222909e6e182f5eb68441035b84cd0
dadlo <dadlobr@gmail.com>
2015-06-15 15:33:00 -0300

    Remove parte não usada nos testes

--- src/prodam/portal/tests/test_viewlets.py
+++ src/prodam/portal/tests/test_viewlets.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from Products.CMFPlone.utils import safe_unicode
 from prodam.portal.browser.viewlets.analytics import AnalyticsViewlet
 from prodam.portal.testing import INTEGRATION_TESTING
 
@@ -15,9 +14,6 @@ class AnalyticsViewletTestCase(unittest.TestCase):
         self.request = self.layer['request']
         self.qi = self.portal['portal_quickinstaller']
         self.st = self.portal['portal_setup']
-        ptool = self.portal['portal_properties']
-        snippet = safe_unicode(ptool.site_properties.webstats_js)
-        snippet = u'<script> analytics </script>'
 
     def viewlet(self):
         viewlet = AnalyticsViewlet(self.portal, self.request, None, None)
