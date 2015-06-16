9db65ab3059ce0a4350fa48267f72f98e95c8dc9
dadlo <dadlobr@gmail.com>
2015-06-16 11:51:53 -0300

    Adiciona tema em sources e tema padrão aplicado na instalação

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'prodam.tema',
         'collective.cover',
         'collective.jsonmigrator',
         'collective.nitf',
--- src/prodam/portal/config.py
+++ src/prodam/portal/config.py
@@ -2,38 +2,6 @@
 
 PROJECTNAME = 'prodam.portal'
 
-DEPS = [
-    'archetypes.querywidget',
-    'collective.googleanalytics',
-    'collective.js.galleria',
-    'collective.js.jqueryui',
-    'plone.app.blocks',
-    'plone.app.collection',
-    'plone.app.contenttypes',
-    'plone.app.dexterity',
-    'plone.app.drafts',
-    'plone.app.intid'
-    'plone.app.intid',
-    'plone.app.iterate',
-    'plone.app.jquery',
-    'plone.app.jquerytools',
-    'plone.app.querystring',
-    'plone.app.relationfield',
-    'plone.app.theming',
-    'plone.app.tiles',
-    'plone.app.versioningbehavior',
-    'plone.formwidget.autocomplete',
-    'plone.formwidget.contenttree',
-    'plone.formwidget.querystring',
-    'plone.resource',
-    'plone.session',
-    'plonetheme.classic',
-    'Products.Doormat',
-    'Products.PloneFormGen',
-    'raptus.autocompletewidget',
-]
-
-
 # http://www.tinymce.com/wiki.php/Configuration:formats
 TINYMCE_JSON_FORMATS = {'strikethrough': {'inline': 'span',
                                           'classes': 'strikethrough',
