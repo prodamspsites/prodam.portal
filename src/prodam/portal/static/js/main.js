ccc44bad097c5495c9abf5fd269986eaa2e0c49a
mosesjr <shank.sa@gmail.com>
2015-08-07 12:31:26 -0300

    Cria javascript do portlet de carrossel

--- src/prodam/portal/static/js/main.js
+++ src/prodam/portal/static/js/main.js
@@ -1,5 +1,14 @@
 (function($) {
    $(document).ready(function() {
+     $("#clickVideo a").bind("click", function () {
+        event.preventDefault();
+        var thisItem = $(this);
+        var thisTitle = $(thisItem).attr('title');
+        var thisLink = $(thisItem).attr('rel');
+        var video = '<object width="290" height="203"><param name="movie" value="'+thisLink+'"><param name="allowScriptAccess" value="always"><embed src="'+thisLink+'" type="application/x-shockwave-flash" allowscriptaccess="always" width="290" height="203"></object>';
+        $('.destaque').html(video);
+        $('#tituloVideo').text(thisTitle)
+     })
      $(".noticias_actions a").bind("click", function () {
         event.preventDefault();
         var size = parseInt($('.contentBody').css("font-size"));
