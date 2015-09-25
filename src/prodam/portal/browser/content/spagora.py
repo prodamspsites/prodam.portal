8655dca0465dad372497248fd662f74ce9305a65
mosesjr <shank.sa@gmail.com>
2015-09-25 16:14:04 -0300

    Adiciona browserview para São Paulo Agora

---
+++ src/prodam/portal/browser/content/spagora.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from Products.Five import BrowserView
+
+
+class governoMunicipal(BrowserView):
+
+    def getTransito(self):
+        secretarias = ''
+        return secretarias
