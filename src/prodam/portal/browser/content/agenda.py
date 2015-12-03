0722742f0f44277e47348c6bb67a906ce5513396
mosesjr <shank.sa@gmail.com>
2015-12-03 15:44:02 -0200

    Finaliza funcionalidades da browserview de agenda

--- src/prodam/portal/browser/content/agenda.py
+++ src/prodam/portal/browser/content/agenda.py
@@ -4,24 +4,31 @@ from DateTime import DateTime
 
 
 class Agenda(BrowserView):
-    def getDate(self):
-        try:
-            requested_date = self.form["date"]
-            start_date = DateTime(requested_date)
-            end_date = DateTime(requested_date + ' 23:59:59')
-            events = self.context.portal_catalog(portal_type='Event',
-                                                 start={'query': start_date,
-                                                        'range': 'min'},
-                                                 end={'query': end_date,
-                                                      'range': 'max'},
-                                                 sort_on='start',
-                                                 review_state='published')
-            return events
-        except:
-            return False
 
     def getDay(self):
         try:
             return self.form["date"]
         except:
-            return False
+            return DateTime().strftime('%m/%d/%Y')
+
+    def getEvents(self):
+        requested_date = self.getDay()
+        start_date = DateTime(requested_date)
+        end_date = DateTime(requested_date + ' 23:59:59')
+        events = self.context.portal_catalog(portal_type='Event',
+                                             start={'query': start_date,
+                                                    'range': 'min'},
+                                             end={'query': end_date,
+                                                  'range': 'max'},
+                                             sort_on='start',
+                                             review_state='published')
+        return events
+
+    def getTitle(self):
+        requested_date = DateTime(self.getDay())
+        title = ''
+        isToday = requested_date.isCurrentDay()
+        title = isToday and 'HOJE: ' or title
+        title += requested_date.strftime('%A, %d de %B de %Y')
+
+        return title
