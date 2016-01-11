8f7b87044692633fc810988971aa73c1189bb308
mosesjr <shank.sa@gmail.com>
2016-01-11 15:06:25 -0200

    Ajusta code analysis do agenda.py

--- src/prodam/portal/browser/content/agenda.py
+++ src/prodam/portal/browser/content/agenda.py
@@ -2,6 +2,7 @@
 from Products.Five import BrowserView
 from DateTime import DateTime
 
+
 class Agenda(BrowserView):
     pass
 
@@ -10,7 +11,6 @@ class Agenda(BrowserView):
             requested_date = self.request.form["date"]
             start_date = DateTime(requested_date)
             end_date = DateTime(requested_date + ' 23:59:59')
-            date_range_query = { 'query':(start_date,end_date), 'range': 'min:max'}
             events = self.context.portal_catalog(portal_type='Event',
                                                  start={'query': start_date,
                                                         'range': 'min'},
