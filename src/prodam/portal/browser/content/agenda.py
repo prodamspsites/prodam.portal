1101ac63c834833ea6a9717ba69c839424ebe287
mosesjr <shank.sa@gmail.com>
2016-03-17 07:46:26 -0300

    Atualiza browserview de agenda

--- src/prodam/portal/browser/content/agenda.py
+++ src/prodam/portal/browser/content/agenda.py
@@ -14,10 +14,11 @@ class Agenda(BrowserView):
 
     def getEvents(self):
         requested_date = self.getDay()
-        start_date = DateTime(requested_date)
+        start_date = DateTime(requested_date + ' 00:00')
+        end_date = DateTime(requested_date + ' 23:59')
         events = self.context.portal_catalog(portal_type='Event',
-                                             start={'query': start_date,
-                                                    'range': 'min'},
+                                             start={'query': [start_date, end_date],
+                                                    'range': 'min:max'},
                                              sort_on='start',
                                              review_state='published')
         return events
