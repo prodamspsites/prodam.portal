2887ec7289eb9de9fcf358b21b35ef9aba91d19a
prodam <prodam@localhost.localdomain>
2016-12-27 12:59:48 -0500

    Ajustes no rodape: Criacao do link 'agenda do prefeito'

--- src/prodam/portal/setuphandlers.py
+++ src/prodam/portal/setuphandlers.py
@@ -77,7 +77,8 @@ def createGovernoMunicipal(site):
 
     url_site = site.absolute_url() + '/agenda'
     # createDoc(site, 'governo-municipal', 'Governo Municipal', 'rodape/governo-municipal', '<ul><li><span>Prefeito</span> <strong>João Dória</strong></li><li><a target="_blank" href="http://www.prefeitura.sp.gov.br/guiadeservicos/content/equipe-de-governo"><strong>Equipe de Governo</strong></a></li></ul><ul class="lista"><li><a href="../../agenda">Agenda do prefeito</a></li></ul>')
-    createDoc(site, 'governo-municipal', 'Governo Municipal', 'rodape/governo-municipal', '<ul><li><span>Prefeito</span><strong>Fernando Haddad</strong></li><li><a target="_blank" href="http://www.prefeitura.sp.gov.br/guiadeservicos/content/equipe-de-governo"><strong>Equipe de Governo</strong></a></li></ul><ul class="lista"><li><a href="' + url_site + '">Agenda do prefeito</a></li></ul>')
+    createDoc(site, 'governo-municipal', 'Governo Municipal', 'rodape/governo-municipal', '<ul class="lista"><li><span>Prefeito</span><strong>Fernando Haddad</strong></li><li><a target="_blank" href="http://www.prefeitura.sp.gov.br/guiadeservicos/content/equipe-de-governo"><strong>Equipe de Governo</strong></a></li></ul>')
+    createLinkObject(site, 'agenda-prefeito', 'Agenda do prefeito', 'rodape/governo-municipal', url_site)
 
 
 def createAconteceNaCidade(site):
